bb3322d1fb80c25bb11c64393846635184a65986
ParameeDilanka <parameeweesinghe@gmail.com>
2025-04-11 11:23:25 +0530

    My Account Page Automation

--- src/com/selenium/ecomerce/automation/MyAccountsPage.java
+++ src/com/selenium/ecomerce/automation/MyAccountsPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class MyAccountPage {
+public class MyAccountsPage {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "D:/Selenium JARS and Drivers/chromedriver-win64/chromedriver-win64/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
